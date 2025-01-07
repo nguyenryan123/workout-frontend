3386e5516bb32ed720e576240348d8bf1a085944
nguyenryan123 <nguyenryan208@gmail.com>
2025-01-06 19:25:01 -0600

    login form hides when logged in

--- workout-frontend/main.js
+++ workout-frontend/main.js
@@ -1,14 +1,15 @@
 const app = Vue.createApp({
     data(){
         return{
-            message: ''
+            message: '',
+            loggedIn: false
         }
     },
     methods:{
         getAccounts(){
             axios
                 .get("/accounts")
-                .then(function (response){
+                .then((response) => {
                     console.log(response)
                 })
         },
@@ -16,6 +17,7 @@ const app = Vue.createApp({
             localStorage.removeItem('name')
             localStorage.removeItem('userid')
             this.updateMessage()
+            this.updateLoggedIn()
         },
         updateMessage(){
             const name = localStorage.getItem('name')
@@ -25,6 +27,18 @@ const app = Vue.createApp({
                 this.message = 'hi ' + name + ' // id: ' + userid
             }
             else this.message = ''
+            
+            this.updateLoggedIn()
+        },
+        updateLoggedIn(){
+            const name = localStorage.getItem('name')
+            const userid = localStorage.getItem('userid')
+            if(name && userid){
+                this.loggedIn = true
+            }
+            else this.loggedIn = false
+
+            console.log(this.loggedIn)
         }
     },
     created(){
